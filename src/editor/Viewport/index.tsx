7d1a84c0de1934ab3329e8befd1251e536dfa8ff
ped <dong.peng01@xgimi.com>
2022-12-02 17:23:21 +0800

    feat: 原型设计

--- src/editor/Viewport/index.tsx
+++ src/editor/Viewport/index.tsx
@@ -1,4 +1,13 @@
-import { FC, useEffect, useRef, useState, useCallback, MouseEvent } from 'react'
+import {
+  createContext,
+  useContext,
+  FC,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  MouseEvent,
+} from 'react'
 import { Box } from '@chakra-ui/react'
 import { ImageElement } from '@/core/Element/ImageElement'
 import { PsyduckNode } from '@/core/Renderer'
@@ -6,6 +15,12 @@ import { event } from '@/editor/event'
 import { PsyduckElement } from '@/core/Element'
 import { effect, stop } from '@vue/reactivity'
 import { useUpdate } from '@/hooks/useUpdate'
+import './style.scss'
+
+const ViewportContext = createContext({
+  selectId: '',
+  setSelectId: (id: string) => {},
+})
 
 const readValue = (obj: Record<string, unknown>) =>
   Object.values(obj).forEach((v) => v)
@@ -15,6 +30,7 @@ const EditBlock = function <T>({
 }: {
   element: PsyduckElement<T | {}>
 }) {
+  const { selectId, setSelectId } = useContext(ViewportContext)
   const nodeRef = useRef<HTMLDivElement | null>(null)
   const update = useUpdate()
   const noWrap = false
@@ -33,6 +49,7 @@ const EditBlock = function <T>({
   const handleClick = useCallback(
     (e: MouseEvent) => {
       // 展示控制
+      setSelectId(node.id)
       event.emit('select-node', node as PsyduckNode<unknown>)
     },
     [node],
@@ -42,13 +59,17 @@ const EditBlock = function <T>({
 
   if (noWrap) return <ElementView {...node.data} />
 
+  const select = selectId === node.id
+
   return (
     <div
       data-pid={node.id}
       ref={nodeRef}
       onClick={handleClick}
+      className='psyduck-edit-block'
       style={{
         cursor: 'pointer',
+        outline: select ? '1px solid red' : '',
       }}
     >
       <ElementView {...node.data} />
@@ -57,11 +78,19 @@ const EditBlock = function <T>({
 }
 
 export const Viewport: FC = () => {
+  const [selectId, setSelectId] = useState('')
   return (
-    <Box h={812} w={375} bg={'#fff'} m='0 auto'>
-      <EditBlock element={ImageElement} />
-      <EditBlock element={ImageElement} />
-      <EditBlock element={ImageElement} />
-    </Box>
+    <ViewportContext.Provider
+      value={{
+        selectId,
+        setSelectId,
+      }}
+    >
+      <Box h={812} w={375} bg={'#fff'} m='0 auto'>
+        <EditBlock element={ImageElement} />
+        <EditBlock element={ImageElement} />
+        <EditBlock element={ImageElement} />
+      </Box>
+    </ViewportContext.Provider>
   )
 }
